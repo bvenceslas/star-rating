8010615af9e26fbab30a34e7c9318fd8ad026674
bvenceslas <bvenceslas@gmail.com>
2023-11-05 09:09:09 +0200

    render stars on the screen

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App rating={5} />
     <App rating={10} />
+    <App />
   </React.StrictMode>
 );
