4a4434643f297b07f09137240cf317316969b98c
bvenceslas <bvenceslas@gmail.com>
2023-11-07 09:09:09 +0200

    create rating

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App rating={5} />
-    <App rating={10} />
-    <App />
+    <App maxRating={5} />
   </React.StrictMode>
 );
