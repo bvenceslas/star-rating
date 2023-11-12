8f7e0e77d5f21e9165e0041b9e188a08f12ed94f
bvenceslas <bvenceslas@gmail.com>
2023-11-12 09:09:09 +0200

    add props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import StartRating from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App maxRating={5} />
+    <StartRating maxRating={5} />
+    <StartRating maxRating={5} size={24} color="red" />
   </React.StrictMode>
 );
