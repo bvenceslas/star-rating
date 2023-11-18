913b52c63a5b037320b2ce1f543f168f96e2d757
bvenceslas <bvenceslas@gmail.com>
2023-11-18 09:09:09 +0200

    use of prop types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,14 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StartRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
 };
 
 function StartRating({
